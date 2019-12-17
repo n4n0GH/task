48ab1546d4720a018fbc6f9703213cbc0ea1a2ab
n4n0GH <n4n0@tuta.io>
2019-12-17 11:24:18 +0100

    fixed issue where an 'in n days' wouldn't be caught as a group and throughout the task entry, also grouping now works for displaying tasks based on their due date

--- task.py
+++ task.py
@@ -9,17 +9,17 @@ from sys import stdout
 from sys import exit as byebye
 
 # global variables for reuse
-appName = "Task"
-dirName = "hello-task"
-homeDir = os.path.expanduser("~")
-targetDir = homeDir + "/.local/share/" + dirName
-targetFile = targetDir + "/tasks.json"
-data = {}
+appName = "Task"                                    # Name of the application
+dirName = "hello-task"                              # Directory name
+homeDir = os.path.expanduser("~")                   # Use home as base
+targetDir = homeDir + "/.local/share/" + dirName    # Determine target directory
+targetFile = targetDir + "/tasks.json"              # Construct full path
+data = {}                                           # JSON written to file
 data["tasks"] = []
 data["settings"] = []
-updateMsg = ""
-idCounter = 1
-unixDay = 86400
+updateMsg = ""                                      # Feedback messages
+idCounter = 1                                       # Used to generate task id
+unixDay = 86400                                     # Used to generate timestamp
 
 
 # function is used by read and write functions
@@ -57,21 +57,22 @@ def jsonWrite(n):
     # it's important to 'try' otherwise entries that don't end in
     # the search string will cause massive errors
     try:
-        dueTime = re.search(r'in\s+(.+?)\s+day(s\b|\b)', n, re.M|re.I)
+        dueTime = re.search(r'(in\s+(\d+?)\s+day(s\b|\b))$', n, re.M|re.I)
         dueTemp = re.search(r'(\d+)', dueTime.group(), re.M|re.I)
         task = n[:-(len(dueTime.group())+1)]
         data["tasks"].append({
             "id": idCounter,
             "task": task,
             # we need to reduce the due time by one second to prevent
-            # the timer showing a wrong due date after creation
+            # the timer showing a wrong due date right after creation
             "due": timeGrab()+(int(dueTemp.group())*unixDay-1)
         })
+    # if there's no due date supplied, write the task to json without
+    # the due key/value pair
     except:
-        task = n
         data["tasks"].append({
             "id": idCounter,
-            "task": task
+            "task": n
         })
     idCounter += 1
     data["settings"][0]["idCounter"] = idCounter
@@ -101,33 +102,69 @@ def jsonRemove(n):
     taskList(targetFile)
 
 
-# read JSON file into memory and print to stdout
+# read JSON file into memory and print to stdout as sorted groups
 def jsonRead(content):
     global data
     global idCounter
     global updateMsg
+    group = {}
+    gkey = ""
+    gval = ""
+    glvl = 0
     with open(content) as objects:
         data = json.load(objects)
     if idCounter <= 1:
         idCounter = data["settings"][0]["idCounter"]
-    # this needs to sort o into lists for further displaying
-    # as grouped items according to due date
+    # let's get things sorted
     for o in data["tasks"]:
-        print(("[" + str(o["id"]) + "] " + o["task"]))
+        # try if it's possible to grab the due key and assign o to a group
         try:
             days = int(o["due"]) - timeGrab()
             days = days/24/60/60+1
             if days < 0:
-                print("Overdue\n")
+                gid = 1
+                gkey = "overdue"
+                gval = "Expired"
+                glvl = 3
             elif days < 1:
-                print("Due today\n")
+                gid = 3
+                gkey = "today"
+                gval = "Due today"
+                glvl = 1
             elif days < 2:
-                print("Due tomorrow\n")
+                gid = 4
+                gkey = "tomorrow"
+                gval = "Due tomorrow"
+                glvl = 1
             else:
-                print(("Due in " + str(int(days)) + " days\n"))
+                gid = 5
+                gkey = str(int(days))
+                gval = "Due in " + gkey + " days"
+                glvl = 3  
+        # if there's no timestamp to use, put o into the "whenever" group
         except:
-            print("Whenever you feel like it\n")
-
+            gid = 2
+            gkey = "whenever"
+            gval = "Whenever you feel like it"
+            glvl = 2
+        # create groups dynamically based on the existence of keys
+        if gkey not in group:
+            group[gkey] = []
+            group[gkey].append({
+                "gid": gid,
+                "due": gval,
+                "lvl": glvl,
+                "item": []
+            })
+        # add tasks to their group keys
+        group[gkey][0]["item"].append(str(o["id"]) + ": " + str(o["task"]))
+    # and finally print everything to the terminal
+    for (k, v) in group.items():
+        for o in v:
+            print(o["due"])
+            for t in o["item"]:
+                print(t)
+            print("")
 
 # display JSON content as task list 
 def taskList(tasks):
@@ -142,16 +179,18 @@ def taskList(tasks):
 # await user input and add or remove tasks
 def userInput():
     global updateMsg
-    print("Type 'help' or '?' for more info")
+    print("Type ':help' or ':?' for more info")
     choice = input("> ").strip()
-    if (choice.lower() == "help") or (choice == "?"):
+    if (choice == ":help") or (choice == ":?"):
         userHelp()
-    elif (choice.lower() == "quit") or (choice.lower() == "exit"):
+    elif choice == ":reset":
+        settingsUpdate(2, "foo")
+    elif (choice == ":quit") or (choice == ":exit"):
         byebye
     elif choice.startswith(":d"):
         jsonRemove(choice[2:].strip())
     elif choice.startswith(":lvl"):
-        settingsUpdate(choice[1:4], choice[4:5])
+        settingsUpdate(1, choice[4:5])
     elif choice == "":
         updateMsg = "[?] Not sure what to do"
         taskList(targetFile)
@@ -162,7 +201,13 @@ def userInput():
 # update user settings
 def settingsUpdate(m, n):
     global updateMsg
-    updateMsg = "[+] View level at " + n
+    if m == 1:
+        # change items shown depending on view level
+        updateMsg = "[+] View level at " + n
+    elif m == 2:
+        # grab the last used id inside the JSON
+        # and set counter to that +1
+        updateMsg = "[+] Counter reset"
     taskList(targetFile)
 
 
@@ -186,9 +231,9 @@ def userHelp():
     print("Once a task is finished, you can use it's id and delete it by typing ':d id' where 'id' would be the number displayed with the task.")
     print("""\nAvailable commands are:
         
-        :d(id)    - Remove a task by ID
-        help/?    - View this screen
-        quit/exit - exit the application""")
+        :d(id)        - Remove a task by ID
+        :help, :?     - View this screen
+        :quit, :exit  - exit the application""")
     input("\nPress return to go back...")
     taskList(targetFile)
 
